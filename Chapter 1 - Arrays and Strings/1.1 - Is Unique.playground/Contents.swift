6bcb2d811369a6b1c24be761946ebee99c208a02
Dave Alton <dave@ovuline.com>
2017-07-21 20:31:24 -0700

    Add big O comments

--- Chapter 1 - Arrays and Strings/1.1 - Is Unique.playground/Contents.swift
+++ Chapter 1 - Arrays and Strings/1.1 - Is Unique.playground/Contents.swift
@@ -1,8 +1,8 @@
 // Implement an algorithm to determine if a string as all unique characters.
 func isUnique(string: String) -> Bool {
     var memo = [Character]()
-    for char in string.characters {
-        if memo.contains(char) {
+    for char in string.characters { // O(N)
+        if memo.contains(char) {    // O(N) (This makes it O(N^2))
             return false
         } else {
             memo.append(char)
@@ -30,9 +30,9 @@ isUnique(string: "a")
 // Ok, I cheated a little on this one. In anticipation of Swift 4, 
 // I created a character array much like how strings can be used in the future.
 func isUniqueNoDataStructures(string: String) -> Bool {
-    let charArray = string.characters.flatMap { $0 }
-    for a in charArray.enumerated() {
-        for bIndex in (a.offset + 1)..<charArray.count {
+    let charArray = string.characters.flatMap { $0 }      // O(N)
+    for a in charArray.enumerated() {                     // O(N)
+        for bIndex in (a.offset + 1)..<charArray.count {  // O(N) (This makes it O(N^2))
             if a.element == charArray[bIndex] {
                 return false
             }
