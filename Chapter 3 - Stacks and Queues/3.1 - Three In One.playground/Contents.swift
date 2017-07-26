0188f193737484c93b45f9927292e54bfe82c7f8
Dave Alton <dave@ovuline.com>
2017-07-25 20:45:05 -0700

    Fix 3.1

--- Chapter 3 - Stacks and Queues/3.1 - Three In One.playground/Contents.swift
+++ Chapter 3 - Stacks and Queues/3.1 - Three In One.playground/Contents.swift
@@ -42,17 +42,22 @@ class Stack {
         }
         valuesString += "]"
         print(valuesString)
+    }
 }
 
 extension Array {
     func divideInto(numberOfStacks: Int) -> [Stack] {
-        let pivot: Double = Double(self.count / numberOfStacks)
+        let pivot = Double(Double(self.count) / Double(numberOfStacks)).rounded(.up)
         var stackIndex: Int?
         var stacks = [Stack]()
         for item in self.enumerated() {
-            // if the modulp of the offset/pivot is 1 or 0, then it's time to create another stack
-            if Double(item.offset).remainder(dividingBy: pivot) <= 1 {
-                stackIndex = stackIndex != nil ? (stackIndex! + 1) : 0
+            // if the modulo of the offset/pivot is 0, then it's time to create another stack
+            if Double(item.offset).remainder(dividingBy: Double(pivot)) == 0 {
+                if stackIndex == nil {
+                    stackIndex = 0
+                } else {
+                    stackIndex! += 1
+                }
                 stacks.append(Stack())
             }
             stacks[stackIndex!].push(value: item.element)
@@ -61,4 +66,11 @@ extension Array {
     }
 }
 
-[0,1,2,3,4,5,6,7].divideInto(numberOfStacks: 3)
+// [ 2 1 0 ],[ 5 4 3 ],[ 7 6 ]
+[0,1,2,3,4,5,6,7].divideInto(numberOfStacks: 3).forEach({ $0.printValues() })
+
+// [ 2 1 0 ],[ 5 4 3 ],[ 8 7 6 ]
+[0,1,2,3,4,5,6,7,8].divideInto(numberOfStacks: 3).forEach({ $0.printValues() })
+
+// [ 0 ],[ 1 ]
+[0,1].divideInto(numberOfStacks: 3).forEach({ $0.printValues() })
